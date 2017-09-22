baae0c16f6892c5dd4948ce801acee6bf0b8aaa0
arun.apad@gmail.com <arun.apad@gmail.com>
2017-09-22 14:26:40 +0530

    First commit

--- english_dictionary.py
+++ english_dictionary.py
@@ -1,13 +1,32 @@
 import json
-
+#import difflib
+#from difflib import SequenceMatcher
+from difflib import get_close_matches
 data = json.load(open("data.json"))
 
 def definition(w):
     if w in data:
-        return(data[w.lower()])
+        w = w.lower()
+        return(data[w])
+    elif len(get_close_matches(w,data.keys())) > 0:
+        yn = input("Did you mean %s? Enter y for yes and n for no: " % get_close_matches(w,data.keys())[0])
+        if yn.lower() == 'y':
+            return(data[get_close_matches(w,data.keys())[0]])
+        elif yn.lower() == 'n':
+            return("That is not a valid english word!")
+        else:
+            return("Did not recognize the entry :( ")
     else:
-        return("Is that a word?")
+        #print(SequenceMatcher(None, "rain", "rainn").ratio())
+        #print(get_close_matches(w,data.keys()))
+        return("That is not a genuine word!")
 
 word = input("Enter the word: ")
-print(definition(word))
-print("git add")
+output = definition(word)
+if type(output) == list:
+    for o in output:
+        print(o)
+else:
+    print(output)
+#print("git add")
+
